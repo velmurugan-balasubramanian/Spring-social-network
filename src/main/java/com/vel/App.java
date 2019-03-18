6c44267a393d5456cefe63b5768964f18fcf21ba
Velmurugan Balasubramanian <velmurugan1124@gmail.com>
2019-03-19 01:31:17 +0530

    heroku commit

--- src/main/java/com/vel/App.java
+++ src/main/java/com/vel/App.java
@@ -2,17 +2,25 @@ package com.vel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesView;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class,args);
 
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+		
+		return application.sources(App.class);
 	}
 	
 	@Bean
